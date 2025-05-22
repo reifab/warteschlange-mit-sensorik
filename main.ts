8208687d634dbfea2d0590353fd45ef7fb750591
Fabian Reifler <72970313+reifab@users.noreply.github.com>
2025-05-22 21:53:33 +0200

    erste funktionierende Lösung

--- main.ts
+++ main.ts
@@ -1,11 +1,59 @@
+function messeHelligkeitsUnterschied (ledNr: number) {
+    led_strip.setPixelColor(ledNr, neopixel.colors(NeoPixelColors.Black))
+    led_strip.show()
+    h_umgebung = messeMaximum()
+    led_strip.setPixelColor(ledNr, neopixel.colors(NeoPixelColors.White))
+    led_strip.show()
+    h_mitLED = messeMaximum()
+    h_unterschied = h_mitLED - h_umgebung
+    led_strip.setPixelColor(ledNr, neopixel.colors(NeoPixelColors.Black))
+    led_strip.show()
+}
+function messeMaximum () {
+    max = 0
+    for (let Index = 0; Index <= 10; Index++) {
+        max = Math.max(max, smartfeldSensoren.getHalfWord_Visible())
+    }
+    return max
+}
+function messen () {
+    m_list = [0]
+    for (let LedNr = 0; LedNr <= 3; LedNr++) {
+        messeHelligkeitsUnterschied(LedNr)
+        m_list[LedNr] = h_unterschied
+    }
+    return m_list
+}
+let personen = 0
+let m_list: number[] = []
+let max = 0
+let h_unterschied = 0
+let h_mitLED = 0
+let h_umgebung = 0
+let led_strip: neopixel.Strip = null
 smartfeldSensoren.initSunlight()
 smartfeldAktoren.oledInit(128, 64)
-let strip = neopixel.create(DigitalPin.P1, 16, NeoPixelMode.RGB)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-strip.show()
-loops.everyInterval(1000, function () {
-    smartfeldAktoren.oledClear()
-    smartfeldAktoren.oledWriteNum(smartfeldSensoren.getHalfWord_Visible())
-    strip.rotate(1)
-    strip.show()
+led_strip = neopixel.create(DigitalPin.P1, 16, NeoPixelMode.RGB_RGB)
+led_strip.setBrightness(255)
+let list_leermessungen = [0]
+let list_messungen = [0]
+list_leermessungen = messen()
+loops.everyInterval(200, function () {
+    personen = 0
+    if (input.buttonIsPressed(Button.A)) {
+        list_leermessungen = messen()
+    } else {
+        list_messungen = messen()
+        smartfeldAktoren.oledClear()
+        for (let LedNr2 = 0; LedNr2 <= 3; LedNr2++) {
+            let list_leermessungMinusMessung: number[] = []
+            list_leermessungMinusMessung[LedNr2] = list_leermessungen[LedNr2] - list_messungen[LedNr2]
+            smartfeldAktoren.oledWriteNumNewLine(list_leermessungMinusMessung[LedNr2])
+            if (list_leermessungMinusMessung[LedNr2] > 50) {
+                personen += 1
+            }
+        }
+        smartfeldAktoren.oledWriteStr("Personen: ")
+        smartfeldAktoren.oledWriteNumNewLine(personen)
+    }
 })
